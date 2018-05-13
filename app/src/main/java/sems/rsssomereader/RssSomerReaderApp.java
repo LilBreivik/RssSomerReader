64df10c65cb573aa56d522e228ce5e54f2a2167a
dbaer <dustin.baer@stud.uni-due.de>
2018-05-13 17:14:51 +0200

    first commit

---
+++ app/src/main/java/sems/rsssomereader/RssSomerReaderApp.java
@@ -0,0 +1,15 @@
+package sems.rsssomereader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+
+public class RssSomerReaderApp extends Activity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
