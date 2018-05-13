a6fd43993a4048d427b4e234d21d3fe5637d6cf4
dbaer <dustin.baer@stud.uni-due.de>
2018-05-13 18:03:49 +0200

    added the missing activity field

--- app/src/main/java/sems/rsssomereader/RssSomeReaderApp.java
+++ app/src/main/java/sems/rsssomereader/RssSomeReaderApp.java
@@ -3,13 +3,14 @@ package sems.rsssomereader;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Menu;
 
-public class RssSomerReaderApp extends Activity{
+public class RssSomeReaderApp extends Activity {
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
